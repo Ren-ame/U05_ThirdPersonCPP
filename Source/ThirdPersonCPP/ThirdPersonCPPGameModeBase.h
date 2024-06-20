c2f1e6e180f7aa831eb0a7f95fa76065f19e96ef
KimGiBeom <94461395+Ren-ame@users.noreply.github.com>
2024-06-20 15:13:28 +0900

    0620_Init

---
+++ Source/ThirdPersonCPP/ThirdPersonCPPGameModeBase.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThirdPersonCPPGameModeBase.generated.h"
+
+UCLASS()
+class THIRDPERSONCPP_API AThirdPersonCPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
